4380fa96c4674fd37ab57f447e03b69f728f9d26
Refs <lhx1010339808@outlook.com>
2018-03-09 11:54:23 +0800

    .

---
+++ google-maps/build/js/app.js
@@ -0,0 +1,10 @@
+var Startup1 = /** @class */ (function () {
+    function Startup1() {
+    }
+    Startup1.main = function () {
+        console.log('133');
+        return 0;
+    };
+    return Startup1;
+}());
+//# sourceMappingURL=app.js.map
---
+++ google-maps/ts/app.ts
@@ -0,0 +1,7 @@
+class Startup1 {
+    
+    public static main():number {
+        console.log('133');
+        return 0;
+    }
+}
