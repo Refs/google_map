998100a4140efb4f758f44c250bc9058adca79c0
Refs <lhx1010339808@outlook.com>
2018-03-09 09:57:26 +0800

    .

---
+++ google-maps/build/js/build/js/maps.js
@@ -0,0 +1,16 @@
+/**
+ *
+ *
+ */
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('133');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=maps.js.map 
+//# sourceMappingURL=maps.js.map
