7529401e5995adf18bcd332abc056d0cfd31f289
Refs <lhx1010339808@outlook.com>
2018-03-11 10:45:22 +0800

    .

--- google-maps/build/js/app.js
+++ google-maps/build/js/app.js
@@ -1,17 +1,40 @@
 var GoogleMap = /** @class */ (function () {
     function GoogleMap(domSelector) {
         this.domSelector = domSelector;
+        this.markers = [];
         this.mapElement = $(domSelector);
         this.initMap();
     }
     GoogleMap.prototype.initMap = function () {
-        var map = new google.maps.Map(this.mapElement[0]);
-        map.setZoom(4);
-        map.setCenter({
+        var _this = this;
+        this.map = new google.maps.Map(this.mapElement[0]);
+        this.map.setZoom(4);
+        this.map.setCenter({
             lat: 23.3,
             lng: 34.2
         });
+        this.map.addListener('click', function (event) {
+            console.log(event.latLng.lat());
+            _this.addMarker(event.latLng);
+        });
+    };
+    GoogleMap.prototype.addMarker = function (location) {
+        this.marker = new google.maps.Marker({
+            position: location,
+            map: this.map
+        });
+        this.marker.setClickable(true);
+        // this.marker.addListener('click',()=> {
+        //     this.marker.setMap(null);
+        // })
+        console.log(this.marker.getPosition());
+        this.markers.push(this.marker);
     };
     return GoogleMap;
+}());
+var LatLng = /** @class */ (function () {
+    function LatLng() {
+    }
+    return LatLng;
 }());
 //# sourceMappingURL=app.js.map
--- google-maps/ts/app.ts
+++ google-maps/ts/app.ts
@@ -1,18 +1,47 @@
 
 class GoogleMap {
+    map:google.maps.Map;
+    marker:google.maps.Marker;
+    markers:Array<google.maps.Marker> = [ ];
     mapElement:JQuery;
     mapDom: Element;
+    markerPosition:LatLng;
     constructor (public domSelector:string){
         this.mapElement = $(domSelector);
         this.initMap();
     }
 
     initMap (){
-        var map = new google.maps.Map(this.mapElement[0]);
-        map.setZoom(4);
-        map.setCenter({
+        this.map = new google.maps.Map(this.mapElement[0]);
+        this.map.setZoom(4);
+        this.map.setCenter({
             lat:23.3,
             lng: 34.2
         })
+        this.map.addListener('click',(event)=>{
+            console.log(event.latLng.lat());
+            this.addMarker(event.latLng);
+        })
     }
+
+    addMarker (location) {
+        this.marker = new google.maps.Marker({
+            position:location,
+            map: this.map
+        });
+        this.marker.setClickable(true);
+        // this.marker.addListener('click',()=> {
+        //     this.marker.setMap(null);
+        // })
+        console.log(this.marker.getPosition());
+        this.markers.push(this.marker);
+    }
+    // 所有的marker 去监听click事件；
+    // lat
+
+}
+
+class LatLng {
+    lat:number;
+    lng:number;
 }
